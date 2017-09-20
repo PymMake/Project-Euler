547ad33d7e3fbabc3996d93d2e3e49d56c442796
Yao ZW <15603345623@163.com>
2017-09-20 10:38:40 +0800

    Update and rename 7.010001st prime.py to 007_10001st prime.py

--- 007_10001st prime.py
+++ 007_10001st prime.py
@@ -1,12 +1,11 @@
 '''10001st prime
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-
 What is the 10 001st prime number?
 
 第10001个素数
 列出前6个素数，它们分别是2、3、5、7、11和13。我们可以看出，第6个素数是13。
-
 第10,001个素数是多少？'''
+
 import math
 
 def func(x):
