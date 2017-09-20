aecc07d8ce5cdc500a5db3ccab9d7dd7da1f9c80
Yao ZW <15603345623@163.com>
2017-09-20 10:42:38 +0800

    Update and rename 9.0Special Pythagorean triplet.py to 009_Special Pythagorean triplet.py

--- 009_Special Pythagorean triplet.py
+++ 009_Special Pythagorean triplet.py
@@ -1,20 +1,16 @@
 '''Special Pythagorean triplet
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-
 a2 + b2 = c2
 For example, 32 + 42 = 9 + 16 = 25 = 52.
-
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.Find the product abc.
 
 特殊毕达哥拉斯三元组
 毕达哥拉斯三元组是三个自然数a < b < c组成的集合，并满足
-
 a2 + b2 = c2
 例如，32 + 42 = 9 + 16 = 25 = 52。
-
 有且只有一个毕达哥拉斯三元组满足 a + b + c = 1000。求这个三元组的乘积abc。
-
 '''
+
 for a in range(3,335):
     for b in range(a,501):
         c = 1000 - a -b
