aa40fedfd7d37af368769613b3c0a927e9ed2342
Yao ZW <15603345623@163.com>
2017-09-20 10:37:05 +0800

    Update 002_Even Fibonacci numbers.py

--- 002_Even Fibonacci numbers.py
+++ 002_Even Fibonacci numbers.py
@@ -7,6 +7,7 @@ By considering the terms in the Fibonacci sequence whose values do not exceed fo
 斐波那契数列中的每一项都是前两项的和。由1和2开始生成的斐波那契数列前10项为：
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, …
 考虑该斐波那契数列中不超过四百万的项，求其中为偶数的项之和。'''
+
 a,b,y =1,1,0
 while a <= 4000000:
     a,b = b,a+b
