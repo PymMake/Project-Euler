4c424b316d77c438489ee51b845e9fbbe5e6dca6
Yao ZW <15603345623@163.com>
2017-09-20 10:32:20 +0800

    Update and rename 3.1.py to 003_Largest prime factor.py

--- 003_Largest prime factor.py
+++ 003_Largest prime factor.py
@@ -1,4 +1,10 @@
-#600851475143
+'''Largest prime factor
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
+
+最大质因数
+13195的所有质因数为5、7、13和29。
+600851475143最大的质因数是多少？'''
 import math
 sqrt=math.sqrt#开根，运算量减小一个数量级
 
