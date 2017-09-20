f93b2bb6abfc7eadf4eb834ffb38e135d960dfe9
Yao ZW <15603345623@163.com>
2017-09-20 10:37:36 +0800

    Update 004_Largest palindrome product.py

--- 004_Largest palindrome product.py
+++ 004_Largest palindrome product.py
@@ -5,6 +5,7 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 最大回文乘积
 回文数就是从前往后和从后往前读都一样的数。由两个2位数相乘得到的最大回文乘积是 9009 = 91 × 99。
 找出由两个3位数相乘得到的最大回文乘积。'''
+
 L = []
 for a in range(100,1000):
     for b in range(100,1000):
